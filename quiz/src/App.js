959d8c65db7295c46e133a593e1a887770030e07
uday kumar <udaykumar.ab2017@gmail.com>
2021-12-13 07:50:28 +0530

    done

---
+++ quiz/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import QuizPage from './component/QuizPage';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizPage/>
+    </div>
+  );
+}
+
+export default App;
